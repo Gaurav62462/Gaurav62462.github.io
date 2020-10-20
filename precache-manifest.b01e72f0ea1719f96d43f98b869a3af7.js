820c6db90a0537d5830633d20f7a7368c47759a4
Gaurav62462 <gaurav.zestgeek@gmail.com>
2020-10-20 10:40:09 +0530

    Updates

---
+++ precache-manifest.b01e72f0ea1719f96d43f98b869a3af7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14998a4ed498449d4187fc47dd4f20fb",
+    "url": "/Gaurav62462.github.io/index.html"
+  },
+  {
+    "revision": "ee926de71af343e21887",
+    "url": "/Gaurav62462.github.io/static/css/2.54b1eb90.chunk.css"
+  },
+  {
+    "revision": "790d7aedd26222ecbe84",
+    "url": "/Gaurav62462.github.io/static/css/main.9aecea6e.chunk.css"
+  },
+  {
+    "revision": "ee926de71af343e21887",
+    "url": "/Gaurav62462.github.io/static/js/2.79b15622.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/Gaurav62462.github.io/static/js/2.79b15622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "790d7aedd26222ecbe84",
+    "url": "/Gaurav62462.github.io/static/js/main.980a25a6.chunk.js"
+  },
+  {
+    "revision": "3bc60097e0caa0ec9eb9",
+    "url": "/Gaurav62462.github.io/static/js/runtime-main.1305c009.js"
+  },
+  {
+    "revision": "47e7ce2c6aafd7e037d57270768244c7",
+    "url": "/Gaurav62462.github.io/static/media/airflow.47e7ce2c.jpg"
+  },
+  {
+    "revision": "d4d9b172c56ed9978baf746a178ac257",
+    "url": "/Gaurav62462.github.io/static/media/clark.d4d9b172.jpg"
+  },
+  {
+    "revision": "719fefb4c89c646aedd5bb462af9d8ff",
+    "url": "/Gaurav62462.github.io/static/media/database.719fefb4.jpg"
+  },
+  {
+    "revision": "184a7681dfc7ae09341edaafc979da8f",
+    "url": "/Gaurav62462.github.io/static/media/js.184a7681.png"
+  },
+  {
+    "revision": "e2a914671640c420a53efc6c41a5b288",
+    "url": "/Gaurav62462.github.io/static/media/key.e2a91467.jpg"
+  },
+  {
+    "revision": "bd542b1910934f228558e68f92abaca8",
+    "url": "/Gaurav62462.github.io/static/media/nebuli.bd542b19.jpg"
+  },
+  {
+    "revision": "afa512a7236c9554316778936120bda5",
+    "url": "/Gaurav62462.github.io/static/media/slid1.afa512a7.jpg"
+  },
+  {
+    "revision": "ce2674fe8c4f3aa5a092f4006573a6e2",
+    "url": "/Gaurav62462.github.io/static/media/slid2.ce2674fe.jpg"
+  },
+  {
+    "revision": "52588ba21622424297d448386a09388d",
+    "url": "/Gaurav62462.github.io/static/media/soniclift.52588ba2.jpg"
+  },
+  {
+    "revision": "6dd9cb774b05b36d10caf7c3e7cd6fe8",
+    "url": "/Gaurav62462.github.io/static/media/unsplash.6dd9cb77.jpg"
+  },
+  {
+    "revision": "fa6246e61ab37a8400b3d035ec1f82f3",
+    "url": "/Gaurav62462.github.io/static/media/wordpress.fa6246e6.png"
+  }
+]);
